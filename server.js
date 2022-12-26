f0c93fd7f83866e4eed268fa0af93b27dd282750
Vasyl Lozynskyi <Vasyl.llozynskyi@gmail.com>
2022-12-26 18:07:55 +0200

    heroku first step

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+// Serve the static files from the React app
+app.use(express.static(__dirname));
+
+// Handles any requests that don't match the ones above
+app.get('/', (req, res) =>{
+    res.render('/build/index.html');
+});
+
+app.listen(PORT, () =>{
+    console.log("Server running");
+});
+
+
+
+
